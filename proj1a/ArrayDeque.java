8a559ac81384803c0b88019566766253e85b924c
dk363 <xukaungdi465@gmail.com>
2025-05-09 15:56:06 +0800

    fix style

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -10,19 +10,20 @@ public class ArrayDeque<T> {
         items = (T[]) new Object[8];
         front = 0;
         last = 0;
-        /**
-         * last指向下一个元素应该插入的位置
-         * 简化边界条件判断 (front == last)
-         * 如果last指向最后一个非空元素 (last + 1) % items.length == front
-         * 而且如果last = -1时，初始化之后，先调用addFirst，然后调用addLast时从 -1 跳到 0 直接赋值会有错误
-         * 如果没看明白就画个图
-         * 保证左右数组位置能被充分利用
+        /*
+          last指向下一个元素应该插入的位置
+          简化边界条件判断 (front == last)
+          如果last指向最后一个非空元素 (last + 1) % items.length == front
+          而且如果last = -1时，初始化之后，先调用addFirst，然后调用addLast时从 -1 跳到 0 直接赋值会有错误
+          如果没看明白就画个图
+          保证左右数组位置能被充分利用
          */
         size = 0;
     }
+    // 扩容缩容优化
 
-    private void reItemSize(int newCapacity) {
-
+    private void reSize(int newCapacity) {
+        newCapacity = Math.max(newCapacity, MIN_LENGTH_ARRAY);
         T[] newItems = (T[]) new Object[newCapacity];
         for (int i = 0; i < size; i++) {
             newItems[i] = items[(front + i) % items.length];
@@ -38,25 +39,24 @@ public class ArrayDeque<T> {
 
     public void addFirst(T i) {
         if (size == items.length) {
-            reItemSize(size * 2);
+            reSize(size * 2);
         }
         front = (front - 1 + items.length) % items.length;
-        /** 为什么要加 items.length ?
-         *  items.length 有可能为 0 ，加上 items.length 后取余才可以达成目标
-         *  python中的 range(::-1) 有可能就是这样写的
-         *  front指的是最前面的非空元素，先更新，再赋值
+        /* 为什么要加 items.length ?
+           items.length 有可能为 0 ，加上 items.length 后取余才可以达成目标
+           python中的 range(::-1) 有可能就是这样写的
+           front指的是最前面的非空元素，先更新，再赋值
          */
         items[front] = i;
         size++;
-
     }
 
     public void addLast(T i) {
         if (size == items.length) {
-            reItemSize(size * 2);
+            reSize(size * 2);
         }
-        /**
-         * last指的是最后的非空元素，先赋值 再更新
+        /*
+           last指的是最后的非空元素，先赋值 再更新
          */
         items[last] = i;
         last = (last + 1) % items.length;
@@ -89,7 +89,7 @@ public class ArrayDeque<T> {
         front = (front + 1) % items.length;
         size--;
         while (size > MIN_LENGTH_ARRAY && size < items.length * 0.25) {
-            reItemSize(items.length / 2);
+            reSize(items.length / 2);
         }
         return p;
     }
@@ -103,7 +103,7 @@ public class ArrayDeque<T> {
         items[last] = null;
         size--;
         while (size > MIN_LENGTH_ARRAY && size < items.length * 0.25) {
-            reItemSize(items.length / 2);
+            reSize(items.length / 2);
         }
         return p;
     }
