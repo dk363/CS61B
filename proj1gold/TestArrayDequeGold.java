f52d3072f3d1a9bc3c91ce48206a2afe4bbdabf4
dk363 <xukaungdi465@gmail.com>
2025-05-15 16:05:28 +0800

    add commits

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -3,6 +3,12 @@ import org.junit.Test;
 
 public class TestArrayDequeGold {
     @Test
+    /** there are four methods in AD
+     *  addlast addFirst removeLast removeFirst
+     *  correct is what we want
+     *  methods in students have something wrong
+     *  we need to write a test to find out the incorrect point
+     */
     public void Test1() {
         StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> correct = new ArrayDequeSolution<>();
@@ -11,8 +17,8 @@ public class TestArrayDequeGold {
 
 
         for (int i = 0; i < 100; i++) {
-            if (correct.isEmpty()) {
-                int HeadOrBack = StdRandom.uniform(2);
+            if (correct.isEmpty()) { // correct is what we want, so we can just check it only
+                int HeadOrBack = StdRandom.uniform(2); // 随机决定是在前面还是在后面
                 int addNumber = StdRandom.uniform(1000);
                 if (HeadOrBack == 0) {
                     log.append("addFirst(").append(addNumber).append(")\n");
@@ -26,7 +32,7 @@ public class TestArrayDequeGold {
             } else {
                 Integer studentRemoveNumber = null;
                 Integer correctRemoveNumber = null;
-                int status = StdRandom.uniform(4);
+                int status = StdRandom.uniform(4); // 用status决定随机调用哪一个 method
                 int addNumber = StdRandom.uniform(1000);
                 if (status == 0) {
                     log.append("addFirst(").append(addNumber).append(")\n");
@@ -37,7 +43,7 @@ public class TestArrayDequeGold {
                     student.addLast(addNumber);
                     correct.addLast(addNumber);
                 } else if (status == 2) {
-                    log.append("RemoveFirst()\n");
+                    log.append("RemoveFirst()\n");// 只有在remove* method 中我们才能获得return value 才能比较是否相等
                     studentRemoveNumber = student.removeFirst();
                     correctRemoveNumber = correct.removeFirst();
                     assertEquals(log.toString(), studentRemoveNumber, correctRemoveNumber);
@@ -47,7 +53,6 @@ public class TestArrayDequeGold {
                     correctRemoveNumber = correct.removeLast();
                     assertEquals(log.toString(), studentRemoveNumber, correctRemoveNumber);
                 }
-
             }
         }
     }
