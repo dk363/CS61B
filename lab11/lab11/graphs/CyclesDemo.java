4d8b1189272557a9180c56ee9879577a3f1f05dd
dk363 <xukaungdi465@gmail.com>
2025-07-18 16:57:29 +0800

    完成Cycle 但是不能让非环元素不连接

--- lab11/lab11/graphs/CyclesDemo.java
+++ lab11/lab11/graphs/CyclesDemo.java
@@ -6,6 +6,7 @@ package lab11.graphs;
 public class CyclesDemo {
     /* Identifies a cycle (if any exist) in the given graph, and draws the cycle with
      * a purple line. */
+    /* 识别给定图形中的循环（如果存在），并用紫色线条绘制该循环。 */
     
     public static void main(String[] args) {
         Maze maze = new Maze("lab11/graphs/maze.txt");
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,9 @@
 package lab11.graphs;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *  @author Josh Hug
  */
@@ -9,15 +13,65 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private boolean cycleFound;
+    private Maze maze;
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        Arrays.fill(edgeTo, -1);
+        int startX = 1, startY = 1;
+        s = maze.xyTo1D(startX, startY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
+    }
+
+    private void dfs(int v) {
+        if (cycleFound) {
+            return;
+        }
+
+        marked[v] = true;
+
+        for (int w : maze.adj(v)) {
+            if (cycleFound) {
+                return;
+            }
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                distTo[w] = distTo[v] + 1;
+                dfs(w);
+            } else if (marked[w] && edgeTo[v] != w) {
+                // w 不是 v 的父节点 且 w 已经被访问过了
+                cycleFound = true;
+                List<Integer> cycle = new ArrayList<>();
+
+                int path = v;
+                cycle.add(v);
+                while (path != w) {
+                    path = edgeTo[path];
+                    cycle.add(path);
+                }
+                cycle.add(v);
+
+                Arrays.fill(edgeTo, -1);
+
+                for (int i = 1; i < cycle.size(); i++) {
+                    edgeTo[cycle.get(i)] = cycle.get(i - 1);
+                    announce();
+                }
+                return;
+            }
+        }
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfs(s);
+        announce();
     }
+
 
     // Helper methods go here
 }
